c9f40430947ff550aa3ede0ac4f89c8a1dea9642
N-i-ke <nike.forgithub@gmail.com>
2024-04-21 20:29:55 +0900

    TODOの作成機能と完了機能

---
+++ index.js
@@ -0,0 +1,44 @@
+const onClickAdd = () => {
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  const li = document.createElement("li");
+
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  const p = document.createElement("p");
+  p.className = "todo-item";
+  p.innerText = inputText;
+
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    const moveTarget = completeButton.closest("li");
+    //隣のボタンを削除する
+    completeButton.nextElementSibling.remove()
+    //押されたボタン自身を削除する
+    completeButton.remove();
+      const backButton = document.createElement("button");
+      backButton.innerText = "戻す"
+      moveTarget.firstElementChild.appendChild(backButton);
+      document.getElementById("complete-list").appendChild(moveTarget)
+  });
+
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    //削除対象を取得する
+    const deleteTarget = deleteButton.closest("li")
+    document.getElementById("incomplete-list").removeChild(deleteTarget);
+  });
+
+  div.appendChild(p);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+  li.appendChild(div);
+
+  document.getElementById("incomplete-list").appendChild(li);
+};
+
+document.getElementById("add-button").addEventListener("click", onClickAdd);
